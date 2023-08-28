378425f0b7dbbf740cd25a95fa031f383ac5d5e3
fazlyalahiru <fazlyalahi.ru@gmail.com>
2023-08-28 21:38:53 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,16 @@
+// imports 
+require('dotenv').config()
+const express = require("express")
+const mongoose = require("mongoose")
+const session = require("express-session")
+
+const app = express()
+const PORT = process.env.PORT || 4001; 
+
+app.get("/", (req, res)=>{
+    res.send('Hello world')
+})
+
+app.listen(PORT, ()=>{
+    console.log(`server running on http://localhost:${PORT}`);
+})
